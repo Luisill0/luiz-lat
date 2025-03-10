6de95084b5f6331a86dd019748bb8c88c42043d4
Luisill0 <luisnaranjo@duck.com>
2025-03-10 14:47:02 -0600

    about section

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,14 @@
 // Animation by Vera Ri - https://www.behance.net/gallery/74597569/Dino-animation. All rights reserved
 export { default as DinoWalking } from "./dino_animaton_by_Vera_Ri.gif";
+
+type ImportedImage = {
+  default: string;
+  "Symbol(Symbol.toStringTag)": string;
+};
+
+export const Gallery: Array<ImportedImage> = Object.values(
+  import.meta.glob("./gallery/*.webp", {
+    eager: true,
+    query: "?url",
+  })
+);
