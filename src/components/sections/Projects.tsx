9b2287ed27699bdc326f620ae8fe9193cdb41185
Luisill0 <luisnaranjo@duck.com>
2025-03-20 22:19:07 -0600

    projects section

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -1,4 +1,8 @@
-import { FC, useCallback, useContext } from "react";
+import { FC, useCallback, useContext, useEffect } from "react";
+
+import { EmblaOptionsType } from "embla-carousel";
+import useEmblaCarousel from "embla-carousel-react";
+import AutoScroll from "embla-carousel-auto-scroll";
 import { Button, ButtonProps } from "primereact/button";
 
 import { Companies } from "assets";
@@ -20,36 +24,28 @@ import { AnchorProps, ComponentWithChildren } from "interface/html";
 import { BreakpointHeight } from "interface/enum/Breakpoint";
 
 const ProjectsSection: FC = () => {
-  const { currentBreakpoint } = useContext(LayoutContext)!;
-  const { height: breakpointHeight } = currentBreakpoint;
+  const { mobile } = useContext(LayoutContext)!;
+
+  const companies = Companies.map((company) => company.default);
 
   return (
     <div>
       <Section id="projects">
         <SectionTitle title="My work" />
-        <div id="areas" className="mx-5 pt-5">
-          <div
-            className={`
-            p-3
-            flex gap-3
-            rounded-2xl shadow-2xl
-            bg-violet-blue
-            ${breakpointHeight < BreakpointHeight.XL && "h-[70vh]"}
-            ${breakpointHeight >= BreakpointHeight.XL && "h-[75vh]"}
-          `}
-          >
+        <div id="areas" className={`mx-5 ${mobile ? "pt-2" : "pt-5"}`}>
+          <AreaBackground>
             <CSArea
               mainIcon="pi pi-desktop"
               title="Frontend Development"
               description="I've worked with many website-related projects, from complex web
-            applications to landing pages."
+              applications to landing pages."
             >
               <SubArea title="Tools">
                 <MappedIcons icons={Frontend} />
               </SubArea>
               <SubArea title="Design">
                 <p>UX/UI</p>
-                <p>Responsive</p>
+                <p>Responsive Applications</p>
               </SubArea>
             </CSArea>
             <CSArea
@@ -85,7 +81,7 @@ const ProjectsSection: FC = () => {
                 <p>Pentesting</p>
               </SubArea>
             </CSArea>
-          </div>
+          </AreaBackground>
         </div>
       </Section>
       <Section id="recent">
@@ -93,7 +89,7 @@ const ProjectsSection: FC = () => {
           <p className="text-center text-chocolate-cosmos brightness-[125%] text-3xl font-bold">
             Featured Projects
           </p>
-          <div className="pt-5 grid grid-cols-3 grid-rows-2 gap-4">
+          <div className="pt-5 grid grid-cols-1 md:grid-cols-3 gap-4">
             <ProjectShowcase
               title="Object Detector"
               image={Detector}
@@ -269,23 +265,30 @@ const ProjectsSection: FC = () => {
           <p className="text-center text-3xl font-bold text-chocolate-cosmos brightness-[125%]">
             Some awesome companies I've worked with!
           </p>
-          <div className="pt-5 flex justify-around select-none">
-            {Companies.map((company, index) => (
-              <img
-                key={index}
-                src={company.default}
-                className={`
-                  ${breakpointHeight < BreakpointHeight.LG && "h-[5rem]"}
-                  ${breakpointHeight >= BreakpointHeight.LG && "h-[6rem]"}
-                `}
-                draggable={false}
-              />
-            ))}
+          <div className="pt-4">
+            <Carousel slides={companies} options={{ loop: true }} />
           </div>
         </div>
       </Section>
     </div>
   );
+};
+
+const AreaBackground: ComponentWithChildren = ({ children }) => {
+  const { currentBreakpoint, mobile } = useContext(LayoutContext)!;
+  const { height: breakpointHeight } = currentBreakpoint;
+
+  const getClassName = useCallback(
+    () =>
+      mobile
+        ? ""
+        : ` p-3 flex gap-3 rounded-2xl shadow-2xl bg-violet-blue ${
+            breakpointHeight < BreakpointHeight.XL && "h-[70vh]"
+          } ${breakpointHeight >= BreakpointHeight.XL && "h-[75vh]"}`,
+    [breakpointHeight, mobile]
+  );
+
+  return <div className={getClassName()}>{children}</div>;
 };
 
 type CSAreaProps = {
@@ -300,16 +303,27 @@ const CSArea: ComponentWithChildren<CSAreaProps> = ({
   description,
   children,
 }) => {
-  const { currentBreakpoint } = useContext(LayoutContext)!;
+  const { currentBreakpoint, mobile } = useContext(LayoutContext)!;
   const { height: breakpointHeight } = currentBreakpoint;
 
   return (
-    <div id="area" className="py-5 rounded-2xl text-center bg-magnolia w-1/3">
+    <div
+      id="area"
+      className="rounded-2xl text-center bg-magnolia"
+      style={{
+        width: `${mobile ? "100%" : "33%"}`,
+        border: `${
+          mobile ? "1px solid var(--color-chocolate-cosmos)" : "unset"
+        }`,
+        margin: `${mobile ? "1rem 0" : "0"}`,
+        padding: `${mobile ? "1.75rem 0" : "1.25rem 0"}`,
+      }}
+    >
       <div className="flex flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center w-[5rem] h-[5rem] rounded-full bg-violet-blue text-magnolia">
           <i className={`${mainIcon} text-5xl`} />
         </div>
-        <p className="mt-2 text-2xl font-semibold">{title}</p>
+        <p className="mt-2 text-2xl font-semibold max-w-[85%]">{title}</p>
       </div>
       <div
         className={`
@@ -331,7 +345,9 @@ type SubAreaProps = {
 
 const SubArea: ComponentWithChildren<SubAreaProps> = ({ title, children }) => (
   <div className="mt-5 pt-2">
-    <p className="text-lg text-violet-blue font-bold">{title}</p>
+    <p className="text-lg text-violet-blue font-bold max-w-[75%] mx-auto">
+      {title}
+    </p>
     {children}
   </div>
 );
@@ -341,11 +357,11 @@ type MappedIconsProps = {
 };
 
 const MappedIcons: FC<MappedIconsProps> = ({ icons }) => {
-  const { currentBreakpoint } = useContext(LayoutContext)!;
+  const { currentBreakpoint, mobile } = useContext(LayoutContext)!;
   const { height: breakpointHeight } = currentBreakpoint;
 
-  const getIconHeight = useCallback(
-    () =>
+  const getIconHeight = useCallback(() => {
+    const destkop =
       breakpointHeight > BreakpointHeight.LG
         ? 60
         : breakpointHeight <= BreakpointHeight.LG &&
@@ -354,19 +370,20 @@ const MappedIcons: FC<MappedIconsProps> = ({ icons }) => {
         : breakpointHeight <= BreakpointHeight.MD &&
           breakpointHeight > BreakpointHeight.SM
         ? 35
-        : 30,
-    [breakpointHeight]
-  );
+        : 30;
+
+    return mobile ? 45 : destkop;
+  }, [breakpointHeight, mobile]);
 
   return (
-    <div className="pt-3 flex gap-2 items-center justify-center">
+    <div className="pt-3 flex flex-wrap gap-2 items-center max-w-[85%] mx-auto justify-center">
       {icons.map((icon, index) => (
         <a
           key={index}
-          href={icon.href}
+          href={mobile ? undefined : icon.href}
           target="_blank"
           rel="noreferrer"
-          className="hover:brightness-90 hover:opacity-90"
+          className="hover:brightness-90 hover:opacity-90 md:active:brightness-75 md:active:opacity-50"
         >
           <img
             src={icon.src}
@@ -393,7 +410,7 @@ const ProjectShowcase: ComponentWithChildren<ProjectShowcaseProps> = ({
   description,
   children,
 }) => {
-  const { currentBreakpoint } = useContext(LayoutContext)!;
+  const { currentBreakpoint, mobile } = useContext(LayoutContext)!;
   const { height: breakpointHeight } = currentBreakpoint;
 
   const MappedDescription = useCallback(
@@ -405,34 +422,42 @@ const ProjectShowcase: ComponentWithChildren<ProjectShowcaseProps> = ({
     <div
       className="
         p-5
-        grid grid-cols-5
+        grid grid-cols-10 md:grid-cols-5
         border-2 border-violet-blue rounded-3xl
       "
     >
-      <div className="flex justify-center items-center col-span-2">
+      <div className="flex justify-center items-center col-span-3 md:col-span-2">
         <img src={image} className="select-none" draggable={false} />
       </div>
-      <div className="col-span-3 ps-4 flex flex-col justify-between">
+      <div className="col-span-7 md:col-span-3 ps-4 flex flex-col justify-between">
         <div>
           <p
-            className={`
-              ${breakpointHeight < BreakpointHeight.MD && "text-lg"}
-              ${breakpointHeight >= BreakpointHeight.MD && "text-2xl"}
-              font-semibold text-center
-            `}
+            className={`font-semibold text-center 
+              ${
+                mobile
+                  ? "text-2xl"
+                  : `${breakpointHeight < BreakpointHeight.MD && " text-lg"}${
+                      breakpointHeight >= BreakpointHeight.MD && " text-2xl"
+                    }`
+              }`}
           >
             {title}
           </p>
           <div
             className={`
             pt-2 
-            ${breakpointHeight < BreakpointHeight.MD && "text-xs"}
             ${
-              breakpointHeight >= BreakpointHeight.MD &&
-              breakpointHeight < BreakpointHeight.LG &&
-              "text-sm"
+              mobile
+                ? "text-sm pb-2"
+                : `${breakpointHeight < BreakpointHeight.MD && " text-xs"}
+                   ${
+                     breakpointHeight >= BreakpointHeight.MD &&
+                     breakpointHeight < BreakpointHeight.LG &&
+                     " text-sm"
+                   }
+                   ${breakpointHeight >= BreakpointHeight.XL && " text-lg"}
+                `
             }
-            ${breakpointHeight >= BreakpointHeight.XL && "text-lg"}
           `}
           >
             <MappedDescription />
@@ -451,8 +476,54 @@ type LinkedButtonProps = AnchorProps & {
 
 const LinkedButton: FC<LinkedButtonProps> = ({ href, btnProps, ...props }) => (
   <a {...props} href={href} target="_blank" rel="noreferrer">
-    <Button {...btnProps} />
+    <Button
+      {...btnProps}
+      className={`active:bg-white active:text-black ${btnProps.className}`}
+    />
   </a>
 );
+
+type CarouselProps = {
+  slides: Array<string>;
+  options?: EmblaOptionsType;
+};
+
+const Carousel: FC<CarouselProps> = ({ slides, options }) => {
+  const { mobile } = useContext(LayoutContext)!;
+
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+    AutoScroll({ playOnInit: true, speed: mobile ? 1 : 2 }),
+  ]);
+
+  const resumePlaying = useCallback(() => {
+    const autoScroll = emblaApi?.plugins()?.autoScroll;
+    if (!autoScroll) return;
+
+    if (!autoScroll?.isPlaying()) autoScroll?.play();
+  }, [emblaApi]);
+
+  useEffect(() => {
+    emblaApi?.on("pointerUp", resumePlaying);
+    emblaApi?.on("settle", resumePlaying);
+  }, [emblaApi, resumePlaying]);
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((slide, index) => (
+            <div key={index} className="embla__slide">
+              <img
+                className="max-h-[10vh] object-contain"
+                src={slide}
+                draggable={false}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProjectsSection;
